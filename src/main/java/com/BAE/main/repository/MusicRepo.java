1581236c81f1395a8a6a69d5581129a547fa48e3
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-12 17:52:32 +0100

    JO: extended interface class to Jpa repository

--- src/main/java/com/BAE/main/repository/MusicRepo.java
+++ src/main/java/com/BAE/main/repository/MusicRepo.java
@@ -1,8 +1,9 @@
 package com.BAE.main.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MusicRepo {
+public interface MusicRepo extends JpaRepository{
 
 }
