03456d9beb6a76d16f0ab9e61cf7a46f9e335061
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-12 15:46:03 +0100

    JO: generation of columns for table

--- src/main/java/com/BAE/main/entities/Music.java
+++ src/main/java/com/BAE/main/entities/Music.java
@@ -1,5 +1,31 @@
 package com.BAE.main.entities;
 
-public class Music {
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Music {
+	
+    
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+		
+	@Column(nullable = false)
+	private String artistName; 
+	
+	@Column(nullable = false)
+	private String songName; 
+	
+	@Column(nullable = false)
+	private String genre; 
+	
+    
+	@Column(nullable = false)
+	private String releaseDate; 
 }
