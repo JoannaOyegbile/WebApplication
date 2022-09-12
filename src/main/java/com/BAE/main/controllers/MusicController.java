a8c81300a670b60edf6e6a85369ef3c9f69b366b
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-12 16:43:47 +0100

    JO: created post request for create

--- src/main/java/com/BAE/main/controllers/MusicController.java
+++ src/main/java/com/BAE/main/controllers/MusicController.java
@@ -1,5 +1,35 @@
 package com.BAE.main.controllers;
 
-public class MusicController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.BAE.main.entities.Music;
+import com.BAE.main.services.MusicService;
+
+@RestController
+@RequestMapping("/music")
+public class MusicController {
+	
+	private MusicService service;
+	
+	public MusicController(MusicService service) {
+		super();
+		this.service = service;
+	}
+
+	// Post request (CREATE)
+		@PostMapping("/create")
+		public ResponseEntity<Music> create(@RequestBody Music input) {
+			return new ResponseEntity<Music> (service.create(input), HttpStatus.CREATED);
+		} 
 }
