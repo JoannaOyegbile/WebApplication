0b4fa4170819ce1ed58c112fd25a055f1e5266ee
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-12 17:54:40 +0100

    JO: inouted entity and data type of id after extension of Jpa Respository

--- src/main/java/com/BAE/main/repository/MusicRepo.java
+++ src/main/java/com/BAE/main/repository/MusicRepo.java
@@ -3,7 +3,9 @@ package com.BAE.main.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.BAE.main.entities.Music;
+
 @Repository
-public interface MusicRepo extends JpaRepository{
+public interface MusicRepo extends JpaRepository<Music, Long>{
 
 }
