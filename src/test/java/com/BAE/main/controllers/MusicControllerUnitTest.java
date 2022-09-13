5fc0a7cab5130200b1bfd78d8f2897a97c040a6e
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 11:25:10 +0100

    JO: inmported and annotated for MockMvc and object mapper

--- src/test/java/com/BAE/main/controllers/MusicControllerUnitTest.java
+++ src/test/java/com/BAE/main/controllers/MusicControllerUnitTest.java
@@ -1,8 +1,17 @@
 package com.BAE.main.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
 public class MusicControllerUnitTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper; 
 }
