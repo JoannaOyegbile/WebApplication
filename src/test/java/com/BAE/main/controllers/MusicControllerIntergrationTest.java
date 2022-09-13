4f8be47ca1bea2ac4cf0c4b17280b3430245d947
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 10:07:53 +0100

    JO: sql annotation to tell spring about the test data and schema file created

--- src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
+++ src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
@@ -2,9 +2,12 @@ package com.BAE.main.controllers;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest
-@AutoConfigureMockMvc 
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class MusicControllerIntergrationTest {
 
 }
