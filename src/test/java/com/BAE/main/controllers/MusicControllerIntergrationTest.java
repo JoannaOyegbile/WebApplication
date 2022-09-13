68bcc2f99202e79e4e465dde22c3888f7f15e09e
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 10:15:21 +0100

    JO: mockmvc to send our mock request

--- src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
+++ src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
@@ -1,15 +1,20 @@
 package com.BAE.main.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class MusicControllerIntergrationTest {
+	
+	@Autowired
+	private MockMvc mvc; 
 
 }
