397e34175e7b58ae57690471ff27c97b2e62f57e
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 10:25:14 +0100

    JO: new array list for the getAllTest and added music input into the list

--- src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
+++ src/test/java/com/BAE/main/controllers/MusicControllerIntergrationTest.java
@@ -1,13 +1,24 @@
 package com.BAE.main.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.BAE.main.entities.Music;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -21,5 +32,18 @@ public class MusicControllerIntergrationTest {
 	
 	@Autowired
 	private ObjectMapper mapper;
+	
+	@Test
+	public void createTest(){
+		
+	}
+	
+	@Test
+	public void gellAllTest()  {
+		List<Music> result = new ArrayList<>();
+		result.add(new Music(1L, "The Greatest Showman", "This is me", "Musical", "2017"));
+		
+		
+	}
 
 }
