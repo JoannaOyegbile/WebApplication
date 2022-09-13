969a4a179438dc761670166bc58920acd48e05c2
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 12:10:18 +0100

    JO: added a mockBean to mock the repo

--- src/test/java/com/BAE/main/services/MusicServiceUnitTest.java
+++ src/test/java/com/BAE/main/services/MusicServiceUnitTest.java
@@ -1,8 +1,20 @@
 package com.BAE.main.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.BAE.main.repository.MusicRepo;
 
 @SpringBootTest
 public class MusicServiceUnitTest {
+	
+	@Autowired
+	private MusicService service;
+	
+	@MockBean
+	private MusicRepo repo;
+	
+	
 
 }
