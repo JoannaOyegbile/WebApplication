10658ca65fc917ff6d6a8378e32b308907555009
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 11:26:37 +0100

    JO: created a mock service

--- src/test/java/com/BAE/main/controllers/MusicControllerUnitTest.java
+++ src/test/java/com/BAE/main/controllers/MusicControllerUnitTest.java
@@ -2,8 +2,10 @@ package com.BAE.main.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.BAE.main.services.MusicService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
@@ -14,4 +16,7 @@ public class MusicControllerUnitTest {
 	
 	@Autowired
 	private ObjectMapper mapper; 
+	
+	@MockBean
+	private MusicService service;
 }
