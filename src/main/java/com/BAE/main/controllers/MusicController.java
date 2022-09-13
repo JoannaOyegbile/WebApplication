55b56a2798df3a6e242e44796d670aaf2222283f
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 14:22:04 +0100

    JO: updates name

--- src/main/java/com/BAE/main/controllers/MusicController.java
+++ src/main/java/com/BAE/main/controllers/MusicController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.BAE.main.entities.Music;
 import com.BAE.main.services.MusicService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/project")
 public class MusicController {
 	
