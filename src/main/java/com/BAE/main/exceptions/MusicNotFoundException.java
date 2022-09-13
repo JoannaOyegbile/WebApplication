6b56aa4001f36f65c14805fe64b1b7b68e68388b
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 08:24:33 +0100

    JO: annotated the class as a RespponseStatus

--- src/main/java/com/BAE/main/exceptions/MusicNotFoundException.java
+++ src/main/java/com/BAE/main/exceptions/MusicNotFoundException.java
@@ -2,6 +2,9 @@ package com.BAE.main.exceptions;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
 public class MusicNotFoundException extends EntityNotFoundException {
 	
 
