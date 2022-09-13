68acc41516c8bdb83156229b1516b6c198aa24e4
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-13 08:25:46 +0100

    JO: set the code to NOT FOUND within the brackets

--- src/main/java/com/BAE/main/exceptions/MusicNotFoundException.java
+++ src/main/java/com/BAE/main/exceptions/MusicNotFoundException.java
@@ -2,9 +2,10 @@ package com.BAE.main.exceptions;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(code = HttpStatus.NOT_FOUND,)
 public class MusicNotFoundException extends EntityNotFoundException {
 	
 
