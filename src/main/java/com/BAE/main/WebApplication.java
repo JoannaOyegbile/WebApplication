cf9a6b0422b39f82e53cfc379ab489ac987d376f
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-09-15 16:41:38 +0100

    Document folder added

--- src/main/java/com/BAE/main/WebApplication.java
+++ src/main/java/com/BAE/main/WebApplication.java
@@ -3,6 +3,8 @@ package com.BAE.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class WebApplication {
 
